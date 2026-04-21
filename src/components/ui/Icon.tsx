76914428878150dcc61e88b7557e34dddb3750b8
marc <dossagaby@gmail.com>
2026-04-21 22:37:55 +0100

    fixed warnings

--- src/components/ui/Icon.tsx
+++ src/components/ui/Icon.tsx
@@ -11,7 +11,7 @@ const Icon = ({name, size = 10, style = "bg-white"}: IconProps) => {
   const dimension = `${size * 0.25}rem`;
 
   return (<div style={{width: dimension, height: dimension}}>
-    <img className={"w-full h-full rounded-lg p-0.25 " + style} src={iconMap[name.toLowerCase().replaceAll(' ', '')]} alt="" />
+    <img className={"w-full h-full rounded-lg p-px " + style} src={iconMap[name.toLowerCase().replaceAll(' ', '')]} alt="" />
   </div>
   )
 }
