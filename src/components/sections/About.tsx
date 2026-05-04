f586962b286560cbeaedd4ca111c9a4280e2ec57
MarcSmg <dossagaby@gmail.com>
2026-05-04 11:36:18 +0100

    feat: justify content in about section

--- src/components/sections/About.tsx
+++ src/components/sections/About.tsx
@@ -10,7 +10,7 @@ const About = () => {
   return (
     <section ref={ref} id="about" className={`flex flex-col pb-20 w-full`}>
       <h1 className="flex gap-2">About<span className="mb-10 bg-linear-to-r from-brand to-brand-emphasis text-transparent bg-clip-text inline-block w-fit">Me</span></h1>
-      <div className={` ${isVisible ? "animate-fade-in-right" : "opacity-0 -translate-x-20"} duration-300 max-w-200`} >
+      <div className={` ${isVisible ? "animate-fade-in-right" : "opacity-0 -translate-x-20"} duration-300 max-w-200 text-justify`} >
         <p>{aboutContent.education}</p>
         <p>{aboutContent.background}</p>
         <p>{aboutContent.interests}</p>
