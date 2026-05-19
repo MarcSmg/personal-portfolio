60c9623dfb222b302fef70fab1276c704e406457
MarcSmg <dossagaby@gmail.com>
2026-05-19 14:04:14 +0100

    chore: update the skills values

--- src/content/en/skills.ts
+++ src/content/en/skills.ts
@@ -11,14 +11,14 @@ import type { SkillsContent } from "../types";
 export const skillsContent: SkillsContent = {
     languages: [
         {name: "JavaScript", level: 80},
-        {name: "TypeScript", level: 60},
-        {name: "HTML", level: 75},
+        {name: "TypeScript", level: 70},
+        {name: "HTML", level: 70},
         {name: "CSS", level: 70},
         {name: "Python", level: 70},
         {name: "Java", level: 60},
         {name: "C", level: 70},
-        {name: "C++", level: 50},
-        {name: "PHP", level: 55}
+        {name: "C++", level: 55},
+        {name: "PHP", level: 50}
     ],
     frontend: [
         {name: "React", level: 75},
@@ -29,7 +29,7 @@ export const skillsContent: SkillsContent = {
         {name: "Flask", level: 60},
         {name: "Express", level: 60},
         {name: "Laravel", level: 65},
-        {name: "REST API", level: 80}
+        {name: "REST API", level: 75}
     ],
     tools: [
         {name: "Git"},
