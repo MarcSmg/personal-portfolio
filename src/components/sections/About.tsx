0ce915a238a6b231fba8ad63ad2ef47fa7804994
MarcSmg <dossagaby@gmail.com>
2026-05-20 14:49:57 +0100

    feat: Integrate ScrollReveal in About section

--- src/components/sections/About.tsx
+++ src/components/sections/About.tsx
@@ -1,26 +1,51 @@
 import { useContent } from "../../context/ContentContext"
-import { useInView } from "@/hooks/useInVIew";
+import ScrollFloat from "../ui/ScrollFloat";
+import ScrollReveal from "../ui/ScrollReveal";
 
 const About = () => {
-  const {content} = useContent();
+  const { content } = useContent();
   const aboutContent = content.aboutContent;
 
-  const {ref: refTitle, isVisible: isVisibleTitle} = useInView();
-  const {ref: refParagraphs, isVisible: isVisibleParagraphs} = useInView();
-
+  // const {ref: refTitle, isVisible: isVisibleTitle} = useInView();
   return (
-    <section ref={refTitle} id="about" className={`flex flex-col mb-20 w-full`}>
-      <h1 className={`flex gap-2 ${isVisibleTitle ? "animate-fade-in-right" : "opacity-0 -translate-x-20"}`}>
+    <section id="about" className={`flex flex-col mb-20 w-full`}>
+      <ScrollFloat containerClassName={`flex gap-2`}>
         About
-        <span className={`mb-10 bg-linear-to-r from-brand to-brand-emphasis text-transparent bg-clip-text inline-block w-fit`}>Me</span>
-      </h1>
-      <div ref={refParagraphs} className={`duration-300 max-w-200 text-justify ${isVisibleParagraphs ? "animate-fade-in-up" : "opacity-0 translate-y-5"} [animation-delay: 50ms]`} >
-        <p>{aboutContent.education}</p>
-        <p>{aboutContent.background}</p>
-        <p>{aboutContent.interests}</p>
-        <p>{aboutContent.objective}</p>
-      </div>
+        <span className="mb-10 text-brand inline-block w-fit">Me</span>
+      </ScrollFloat>
+      <div className={`max-w-200 text-justify`}>
+        <ScrollReveal
+          textClassName="font-normal text-lg"
+          enableBlur={true}
+          baseRotation={15}
+        >
+          {aboutContent.education}
+        </ScrollReveal>
 
+        <ScrollReveal
+          textClassName="font-normal text-lg"
+          enableBlur={true}
+          baseRotation={15}
+        >
+          {aboutContent.background}
+        </ScrollReveal>
+
+        <ScrollReveal
+          textClassName="font-normal text-lg"
+          enableBlur={true}
+          baseRotation={15}
+        >
+          {aboutContent.interests}
+        </ScrollReveal>
+
+        <ScrollReveal
+          textClassName="font-normal text-lg"
+          enableBlur={true}
+          baseRotation={15}
+        >
+          {aboutContent.objective}
+        </ScrollReveal>
+      </div>
     </section>
   )
 }
